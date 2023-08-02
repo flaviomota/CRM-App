c9b01e748c33beda9223f8f9577f5fcc7172e72e
flaviomota <flaviofilipemota@gmail.com>
2023-08-02 16:10:53 +0100

    Records Card | Display Content and Delete

--- website/views.py
+++ website/views.py
@@ -44,3 +44,22 @@ def register_user(request):
 			return render(request, 'register.html', {'form':form})
 		
 		return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+		if request.user.is_authenticated:
+			# Look Up Records
+			customer_record = Record.objects.get(id=pk)
+			return render(request, 'record.html', {'customer_record':customer_record})
+		else:
+			messages.success(request, "You Must Be Logged In To View That Page...")
+			return redirect('home')
+
+def delete_record(request, pk):
+		if request.user.is_authenticated:
+			delete_it = Record.objects.get(id=pk)
+			delete_it.delete()
+			messages.success(request, "Record Deleted Successfully...")
+			return redirect('home')
+		else:
+			messages.success(request, "You Must Be Logged In To Do That...")
+			return redirect('home')
